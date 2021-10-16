ef93399c3f32afeb7b8f81862eacb948f1a2159a
Paul Barnard <paul.barnard@gmail.com>
2021-10-16 10:15:54 +0100

    Added unit test project

---
+++ Swindler/Swindler.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Shouldly;
+using Xunit;
+
+namespace Swindler.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.ShouldBeTrue();
+        }
+    }
+}
